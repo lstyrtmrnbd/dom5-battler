6c34978256999095312c7dd64db4f2f6b7a2514e
Mule Sknr <soymilk@airmail.cc>
2020-08-13 02:08:45 -0400

    Load data and parse via mod inspector snippet

---
+++ dmi/loaddata.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+function loadData() {
+
+    const dataLocs = {
+        nation: 'gamedata/nations.csv',
+        unit: 'gamedata/BaseU.csv',
+        item: 'gamedata/BaseI.csv'
+    };
+
+    const output = {};
+    
+    Object.keys(dataLocs).forEach(key => {
+        const data = fs.readFileSync(dataLocs[key], {encoding:'utf8'});
+        const parsed = parseTextToTable(data);
+        const name = key === 'nation' ? 'nationname' : 'name';
+        output[key] = createLookup(parsed, 'id', name);
+    });
+
+    return output;
+}
+
+function parseTextToTable(str) {
+    var t = [];
+
+    var lines = str.split("\n");
+    var keynames = lines[0].split("\t");
+
+    for (var i=1; i<lines.length; i++) {
+	var values = lines[i].split("\t");
+
+	if (values[0]=="")
+	    continue;
+
+	var o = new Object();
+	for (var j=0; j<keynames.length; j++) {
+	    var key = keynames[j];
+	    var val = values[j];
+	    if (val != "" && val != "\r") {
+		//if (key == 'id#') key = 'id';
+		o[key] = values[j];
+	    }
+	}
+	t.push(o);
+    }
+    return t;
+}
+
+function createLookup(t, k1, k2) {
+    var lookup = {};
+    for (var i=0; i<t.length; i++) {
+	var line = t[i];
+
+	var v1;
+	if ((v1= line[k1]) && !lookup[v1])
+	    lookup[v1] = line;
+
+	var v2;
+	if (k2 && (v2= line[k2])) {
+	    v2 = v2.toLowerCase();
+	    if (!lookup[v2])
+		lookup[v2] = line;
+	}
+    }
+    return lookup;
+}
+
+module.exports = loadData;
