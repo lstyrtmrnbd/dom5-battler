bad083310e09b55272fa0039b2d73e94a90de2ca
Mule Sknr <soymilk@airmail.cc>
2020-08-13 18:00:28 -0400

    Data loading and embedding

--- preload.js
+++ preload.js
@@ -1,20 +1,48 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-
-const {
-    newArmy,
-    writeOut
-} = require('./batter');
+const {newArmy,writeOut} = require('./battler');
+const {loadDataSync} = require('./dmi/loaddata');
 
 const ARMY1 = {};
 
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector);
-        if (element) element.innerText = text;
-    };
+const mappedUnitToOption = (mappedUnit) => {
+    const {id, name} = mappedUnit;
+    return `<option value="${id}">${name}</option>`;
+};
 
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, 'stupid function');
-    }
+function addData(data) {
+
+    console.log('Now loading...');
+
+    let outHTML = '';
+    
+    Object.keys(data.unit).forEach(key => {
+        outHTML += mappedUnitToOption(data.unit[key]);
+    });
+
+    unitList.innerHTML = outHTML;
+
+    console.log('Done loading');
+}
+
+const DATALOCS = {
+    nation: 'gamedata/nations.csv',
+    unit: 'gamedata/BaseU.csv',
+    item: 'gamedata/BaseI.csv'
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    // load data
+    //sessionStorage.setItem('data', loadData()); // save in sessionstorage
+    
+    const data = loadDataSync(DATALOCS);
+
+    addData(data);
+   
+    // Object.keys(data.unit).forEach(key => {
+    //     unitList.innerHTML += mappedUnitToOption(data.unit[key]);
+    // });
+
+    
 });
